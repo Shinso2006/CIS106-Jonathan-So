af4ad944eaefb3ff28ceefffc85824a3d24c93fe
Shinso2006 <164662349+Shinso2006@users.noreply.github.com>
2026-03-01 23:02:06 -0600

    Create PS3P2.py

---
+++ PS3P2.py
@@ -0,0 +1,8 @@
+last_name = input("Enter student last name: ")
+midterm = float(input("Enter midterm score: "))
+final_exam = float(input("Enter final exam score: "))
+
+total_points = midterm + final_exam
+
+print(f"Student: {last_name}")
+print(f"Total Exam Points: {total_points}")
