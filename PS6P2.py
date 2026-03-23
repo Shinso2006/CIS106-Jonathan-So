90a11c61ace9e8ddf055a79b1c8dd0fc37129ee9
Shinso2006 <164662349+Shinso2006@users.noreply.github.com>
2026-03-22 23:13:36 -0500

    Create PS6P2.py

---
+++ PS6P2.py
@@ -0,0 +1,8 @@
+start = int(input("Enter start value: "))
+stop = int(input("Enter stop value: "))
+inc = int(input("Enter increment value: "))
+
+current = start
+while current <= stop:
+    print(current)
+    current += inc
